756b969ff9c1358ee0edeec63f0b71cbf9c0efab
Liam Blumenstein-Tait <liam.bt@gmail.com>
2026-02-02 22:12:31 +0100

    6.5 & 6.6

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -17,10 +17,13 @@ const App = () => {
     dispatch(createAnecdote(newAnecdote))
   }
 
+  const nextAnecdotes = [...anecdotes]
+  const sortedAnecdotes = nextAnecdotes.sort((a,b) => b.votes - a.votes)
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote => (
+      {sortedAnecdotes.map(anecdote => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
