8b522a258632aec8eca7adeb92ce07b0a4a165f9
Liam Blumenstein-Tait <liam.bt@gmail.com>
2026-02-03 13:00:15 +0100

    fix creating new anecdotes

---
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,30 @@
+import { incrementVote } from '../reducers/anecdoteReducer'
+import { useSelector, useDispatch } from 'react-redux'
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state.anecdotes)
+
+  const vote = (id) => {
+    dispatch(incrementVote(id))
+  }
+
+  const nextAnecdotes = [...anecdotes]
+  const sortedAnecdotes = nextAnecdotes.sort((a,b) => b.votes - a.votes)
+
+  return (
+    <div>
+      {sortedAnecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default AnecdoteList
